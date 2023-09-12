402e9732cff9989ba39afe11214014db82e6f115
atiqNineSol <atiqurrehmanninesol@gmail.com>
2023-09-12 16:30:54 +0500

    first push

---
+++ app/src/main/java/com/example/bottomnavigationwithnav/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.bottomnavigationwithnav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.bottomnavigationwithnav.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding= FragmentHomeBinding.inflate(inflater,container,false)
+        // Inflate the layout for this fragment
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.textClick?.setOnClickListener{
+            findNavController().navigate(R.id.idInnerFragment)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/bottomnavigationwithnav/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.bottomnavigationwithnav
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.bottomnavigationwithnav.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+
+    var binding: ActivityMainBinding? = null
+    private var navController: NavController? = null
+    var navHostFragment: NavHostFragment? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment?.navController
+        navController?.addOnDestinationChangedListener(this)
+
+        binding?.run {
+            navController?.let { binding?.bottomNavView?.setupWithNavController(it) }
+
+            bottomNavView.setOnNavigationItemSelectedListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.fragment_home -> {
+                        // Handle Home item click
+                        navController?.navigate(R.id.idHomeFragment)
+                        true
+                    }
+                    R.id.fragment_more -> {
+                        // Handle Search item click
+                        navController?.navigate(R.id.idMoreFragment)
+
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        }
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController, destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        binding?.run {
+            when (destination.id) {
+                R.id.idHomeFragment, R.id.idMoreFragment -> {
+                    bottomNavView.visibility = View.VISIBLE
+                }
+
+                R.id.idInnerFragment -> {
+                    bottomNavView.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+}
